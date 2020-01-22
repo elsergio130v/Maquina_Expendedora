d5ecb32e74bd1fa05f67a33ec86b1aa3c9e60b39
Adil-1995 <59649428+Adil-1995@users.noreply.github.com>
2020-01-22 13:52:17 +0100

    fff

--- Maquina_Expendedora/src/AdilChair.java
+++ Maquina_Expendedora/src/AdilChair.java
@@ -4,8 +4,7 @@ public class AdilChair {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Motherfuckers");
-		System.out.println("Hello Motherfuckers2");
-		System.out.println("Hello Motherfuckers2");
+		
 
 	}
 
