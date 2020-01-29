5be76ad8080fc4770649edde75ffd64000829285
Adil-1995 <59649428+Adil-1995@users.noreply.github.com>
2020-01-29 08:21:08 +0100

    Add files via upload

---
+++ MaquinaExpendedora/src/SergioCarroza/Producto.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Producto {
+	
+
+	public static void main(String[] args) {
+		
+        
+		
+		String [][] NombreProductos= {
+				{"Agua","Pepsi","Coca Cola",
+				"Patata Lays","pipa","Zumo"}
+		
+	    };
+		int [] [] Precio = {
+				{1 , 2 , 2 , 3 , 1 , 2  } 
+				
+		};
+		 Scanner myScanner = new Scanner (System.in);
+  }
+
+}
